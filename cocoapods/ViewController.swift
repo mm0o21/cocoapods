bc08e70d2fea5423fbe2ba8eb4652d9bf491e6b4
maoko <maaaoko0021@gmail.com>
2022-09-03 08:09:40 +0900

    Initial Commit

---
+++ cocoapods/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cocoapods
+//
+//  Created by Maoko Furuya on 2022/09/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
