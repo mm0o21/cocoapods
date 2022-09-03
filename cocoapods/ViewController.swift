dbf954bf7d1c5a6c85c93ecdb612c8bb4212c60e
maoko <maaaoko0021@gmail.com>
2022-09-03 13:08:51 +0900

    done

--- cocoapods/ViewController.swift
+++ cocoapods/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        HUD.flash(.success, delay: 2.0)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         HUD.flash(.success, delay: 2.0)
     }
 
